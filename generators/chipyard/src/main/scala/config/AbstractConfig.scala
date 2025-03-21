9ea9be72a508d25e6e2307e76258a2bc9086b32d
thhoanq <hoangtrinh140103@gmail.com>
2025-03-21 14:12:29 +0700

    Maybe original AbstractConfig.scala

--- generators/chipyard/src/main/scala/config/AbstractConfig.scala
+++ generators/chipyard/src/main/scala/config/AbstractConfig.scala
@@ -49,7 +49,6 @@ class AbstractConfig extends Config(
   new chipyard.iobinders.WithCustomBootPin ++
   // The "punchthrough" IOBInders below don't generate IOCells, as these interfaces shouldn't really be mapped to ASIC IO
   // Instead, they directly pass through the DigitalTop ports to ports in the ChipTop
-//  new chipyard.iobinders.WithGPIOPunchthrough ++    /** No tie-off GPIOs */
   new chipyard.iobinders.WithI2CPunchthrough ++
   new chipyard.iobinders.WithSPIIOPunchthrough ++
   new chipyard.iobinders.WithAXI4MemPunchthrough ++
